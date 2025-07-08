d34c0ccc1cce6ebebedd4e3da32e670b66c5ae23
TingChiehLin <cooloojayoo@gmail.com>
2025-07-09 01:51:56 +1000

    Add - Todo 5_adjust_results4_isadog

--- app/get_pet_labels.py
+++ app/get_pet_labels.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
-# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
-#          the image's filename. This function inputs: 
-#           - The Image Folder as image_dir within get_pet_labels function and 
-#             as in_arg.dir for the function call within the main function. 
+#
+# PROGRAMMER:
+# DATE CREATED:
+# REVISED DATE:
+# PURPOSE: Create the function get_pet_labels that creates the pet labels from
+#          the image's filename. This function inputs:
+#           - The Image Folder as image_dir within get_pet_labels function and
+#             as in_arg.dir for the function call within the main function.
 #          This function creates and returns the results dictionary as results_dic
-#          within get_pet_labels function and as results within main. 
+#          within get_pet_labels function and as results within main.
 #          The results_dic dictionary has a 'key' that's the image filename and
 #          a 'value' that's a list. This list will contain the following item
 #          at index 0 : pet image label (string).
@@ -19,15 +19,16 @@
 # Imports python modules
 from os import listdir
 
+
 # TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
+#       in the return statement with results_dic dictionary that you create
 #       with this function
-# 
+#
 def get_pet_labels(image_dir):
     """
-    Creates a dictionary of pet labels (results_dic) based upon the filenames 
-    of the image files. These pet image labels are used to check the accuracy 
-    of the labels that are returned by the classifier function, since the 
+    Creates a dictionary of pet labels (results_dic) based upon the filenames
+    of the image files. These pet image labels are used to check the accuracy
+    of the labels that are returned by the classifier function, since the
     filenames of the images contain the true identity of the pet in the image.
     Be sure to format the pet labels so that they are in all lower case letters
     and with leading and trailing whitespace characters stripped from them.
@@ -36,7 +37,7 @@ def get_pet_labels(image_dir):
      image_dir - The (full) path to the folder of images that are to be
                  classified by the classifier function (string)
     Returns:
-      results_dic - Dictionary with 'key' as image filename and 'value' as a 
+      results_dic - Dictionary with 'key' as image filename and 'value' as a
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
@@ -47,11 +48,13 @@ def get_pet_labels(image_dir):
     # Loop through each filename in the folder
     for filename in filenames:
         # Create a pet label from the filename
-        # Split the filename by underscores('_') 
+        # Split the filename by underscores('_')
         # Keeps only words made of alphabet letters
         # Convert to lowercase and join them with spaces
         # Example: 'Boston_terrier_02259.jpg' becomes 'boston terr
-        pet_label = ' '.join([word.lower() for word in filename.split('_') if word.isalpha()])
+        pet_label = " ".join(
+            [word.lower() for word in filename.split("_") if word.isalpha()]
+        )
         # Add the pet label to the results dictionary
         results_dic[filename] = [pet_label]
 
